53f220257160d8e66cda321c00f8ddfb44e51fe9
luca patarca <luca.patarca@studenti.unicam.it>
2020-12-09 21:41:33 +0100

    Created structure of backend

---
+++ Backend/src/main/java/com/github/trionfettinicoUNICAM/PPTeam_DOIT/PpTeamDoitApplication.java
@@ -0,0 +1,13 @@
+package com.github.trionfettinicoUNICAM.PPTeam_DOIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpTeamDoitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpTeamDoitApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/github/trionfettinicoUNICAM/PPTeam_DOIT/PpTeamDoitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.trionfettinicoUNICAM.PPTeam_DOIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpTeamDoitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
