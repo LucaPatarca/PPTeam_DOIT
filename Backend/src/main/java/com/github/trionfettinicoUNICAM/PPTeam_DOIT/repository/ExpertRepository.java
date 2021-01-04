d14f6456c7d2c6672657258ce8ff208db16400b3
nico1798 <nico.trionfetti@studenti.unicam.it>
2021-01-04 17:08:22 +0100

    SD dei casi d'uso inerenti Esperti e Collaboratori

---
+++ Backend/src/main/java/com/github/trionfettinicoUNICAM/PPTeam_DOIT/repository/ExpertRepository.java
@@ -0,0 +1,10 @@
+package com.github.trionfettinicoUNICAM.PPTeam_DOIT.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ExpertRepository  {
+
+}
