99283ced96351616f244ea0ec2d70b26e92c81b5
nico <nico.trionfetti@studenti.unicam.it>
2021-03-08 16:38:53 +0100

    updated home page

--- Frontend/src/app/home/home.page.ts
+++ Frontend/src/app/home/home.page.ts
@@ -1,5 +1,10 @@
+import { ProjectInformation } from 'src/app/model/project-information';
+import { OrganizationInformation } from 'src/app/model/organization-information';
+import { Project } from 'src/app/model/project';
+import { Organization } from 'src/app/model/organization';
+import { RestService } from 'src/app/services/rest.service';
 import { DataService } from 'src/app/services/data.service';
-import { Platform, ModalController } from '@ionic/angular';
+import { Platform, NavController } from '@ionic/angular';
 import { Component } from '@angular/core';
 
 @Component({
@@ -11,21 +16,103 @@ export class HomePage {
 
   HWBackSubscription: any;
 
+  page:number;
+
+  selection:string;
+
+  organizations:Organization[];
+  projects:Project[];
+
   constructor(
     private platform: Platform,
     public dataService:DataService,
+    private restService:RestService,
+    private navCtrl:NavController
   ) {
-    
   }
 
   ionViewDidEnter() {
+    this.organizations = new Array();
+    this.projects = new Array();
     this.HWBackSubscription = this.platform.backButton.subscribe(() => {
       navigator['app'].exitApp();
     });
   }
 
-  ionViewDidLeave(){
+  ionViewDidLeave() {
     this.HWBackSubscription.unsubscribe();
+  }
+
+  // metodo per richiedere una pagina di elementi
+  async loadOrganizations(event?) {
+    this.restService.getUserOrganizations(this.dataService.getUser().mail)
+      .then(res => {
+        this.organizations = this.organizations.concat(res);
+        if (event)
+          event.target.complete();
+      }).catch(err => {
+        this.organizations = new Array();
+        if (event)
+          event.target.complete();
+      });
+  }
+
+  async loadProjects(event?) {
+    this.restService.getUserProjects(this.dataService.getUser().mail)
+      .then(res => {
+        this.projects = this.projects.concat(res);
+        if (event)
+          event.target.complete();
+      }).catch(err => {
+        this.projects = new Array();
+        if (event)
+          event.target.complete();
+    });
+  }
+
+
+  // metodo per aprire la visualizzazione di una pagina (gli si passa un organization)
+  viewOrganization(organizationId: String) {
+    this.navCtrl.navigateForward(['/tabs/list-of-organizations/view-organization', { "id": organizationId }]);
+  }
+
+  viewProject(projectId: String) {
+    this.navCtrl.navigateForward(['/tabs/list-of-projects/view-project', { "id": projectId }]);
+  }
+
+  createOrganization() {
+    this.dataService.modify = null;
+    this.navCtrl.navigateForward(["/tabs/list-of-organizations/create-organization"]);
+  }
+
+  segmentChanged(event: any) {
+    this.organizations = new Array();
+    this.projects = new Array();
+    this.selection = event.detail.value;
+    if(this.selection=="organizations")
+      this.loadOrganizations();
+    else
+      this.loadProjects();
+  }
+
+  isEmpty(): boolean {
+    if (this.selection == "organizations") {
+      return this.organizations.length == 0;
+    } else {
+      return this.projects.length == 0;
+    }
+  }
+
+  getOrganizations():Organization[] {
+    return this.organizations;
+  }
+
+  getProjects():Project[] {
+    return this.projects;
+  }
+
+  isOrganizations():boolean{
+    return this.selection=="organizations";
   }
 
 }
